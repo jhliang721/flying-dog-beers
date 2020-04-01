7630e7651a2e17d03e34e1753f1129827f77ba13
jhliang721 <63000942+jhliang721@users.noreply.github.com>
2020-04-01 23:45:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ df = pandas.DataFrame(articles)
 df['text']
 
 articles = []
-for i in range(1,10):
+for i in range(1,2):
     res = requests.get('https://m.weibo.cn/api/container/getIndex?containerid=231522type%3D1%26q%3D%23%E9%A6%99%E5%A5%88%E5%84%BF%23&page_type=searchall&page={}'.format(i))
     jd = res.json()
     articles.extend( [rec['mblog'] for rec in jd['data']['cards'] if rec.get('mblog')] )
