570eb584e2bc658eff3f95f6302612e53a45534e
jhliang721 <63000942+jhliang721@users.noreply.github.com>
2020-04-02 23:48:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,19 +13,14 @@ app = Flask(__name__)
 @app.route('/')
 def testweibocrawling():
     articles = []
-    for i in range(1,10):
+    for i in range(1,100):
         res = requests.get('https://m.weibo.cn/api/container/getIndex?containerid=231522type%3D1%26q%3D%23%E9%A6%99%E5%A5%88%E5%84%BF%23&page_type=searchall&page={}'.format(i))
     jd = res.json()
     articles.extend( [rec['mblog'] for rec in jd['data']['cards'] if rec.get('mblog')] )
     df = pandas.DataFrame(articles)
     print(len(df))
     a=len(df)
-
-#    print(df.to_html(classes='tbstyle'))
-#     for a in range(1,6):
-#         for b in range(1,62):
-#             print(a + '_' + b)
-    pandas.set_option('max_colwidth',200)
+    pandas.set_option('max_colwidth',500)
     pandas.DataFrame(df)
     print(df.loc[1:a, 'id': 'text'])
     return '<div>' + df.to_html(classes='tbstyle') + '</div>'
