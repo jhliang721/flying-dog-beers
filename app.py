1dbb453a2440db859289ba08d7403b125ee8b521
jhliang721 <63000942+jhliang721@users.noreply.github.com>
2020-04-02 23:23:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 from flask import Flask
-
+    
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+pandas.set_option('max_colwidth',500)
+pandas.DataFrame({'text':[longString]})
+    def testweibocrawling():
     articles = []
     for i in range(1,2):
         res = requests.get('https://m.weibo.cn/api/container/getIndex?containerid=231522type%3D1%26q%3D%23%E9%A6%99%E5%A5%88%E5%84%BF%23&page_type=searchall&page={}'.format(i))
