5afba96b191f016dc0f58cce9512053bb53e8743
jhliang721 <63000942+jhliang721@users.noreply.github.com>
2020-04-02 23:40:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
-    def testweibocrawling():
+def testweibocrawling():
     articles = []
     for i in range(1,10):
         res = requests.get('https://m.weibo.cn/api/container/getIndex?containerid=231522type%3D1%26q%3D%23%E9%A6%99%E5%A5%88%E5%84%BF%23&page_type=searchall&page={}'.format(i))
