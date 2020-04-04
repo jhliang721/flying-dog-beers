41726bade41e0fb858ce68a7bbcbf8875f1859fa
jhliang721 <63000942+jhliang721@users.noreply.github.com>
2020-04-04 18:37:30 +0800

    Update weibo_crawling_v1.py

--- weibo_crawling_v1.py
+++ weibo_crawling_v1.py
@@ -11,13 +11,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def weibo_api_crawling():
-# gary's weibo account
-# https://m.weibo.cn/api/container/getIndex?uid=1764276651&luicode=10000011&lfid=100103type%3D1%26q%3Dkawaaaaaaaa&containerid=1076031764276651&since_id=4079234580721927
-base_url='https://m.weibo.cn/api/container/getIndex?'
-
-weiboaccount = {
-    "containerid":"1076031764276651",
-    "page_type":"03",
-    "page":4
-}
-print(urlencode(weiboaccount))
+    # gary's weibo account
+    # https://m.weibo.cn/api/container/getIndex?uid=1764276651&luicode=10000011&lfid=100103type%3D1%26q%3Dkawaaaaaaaa&containerid=1076031764276651&since_id=4079234580721927
+    base_url='https://m.weibo.cn/api/container/getIndex?'
+    weiboaccount = {
+        "containerid":"1076031764276651",
+        "page_type":"03",
+        "page":4
+    }
+    print(urlencode(weiboaccount))
