989e17e3425749abc446797f09e0d0a689af42fb
jhliang721 <63000942+jhliang721@users.noreply.github.com>
2020-04-04 18:35:49 +0800

    Update weibo_crawling_v1.py

--- weibo_crawling_v1.py
+++ weibo_crawling_v1.py
@@ -3,14 +3,21 @@ import requests
 import pandas
 import numpy
 import plotly.graph_objs as go
+from urllib.parse import urlencoding
 
 from flask import Flask
     
 app = Flask(__name__)
 
 @app.route('/')
-def testweibocrawling():
+def weibo_api_crawling():
 # gary's weibo account
 # https://m.weibo.cn/api/container/getIndex?uid=1764276651&luicode=10000011&lfid=100103type%3D1%26q%3Dkawaaaaaaaa&containerid=1076031764276651&since_id=4079234580721927
 base_url='https://m.weibo.cn/api/container/getIndex?'
-response=requests.get
+
+weiboaccount = {
+    "containerid":"1076031764276651",
+    "page_type":"03",
+    "page":4
+}
+print(urlencode(weiboaccount))
